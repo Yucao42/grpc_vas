217ca47f8796820f9348aef5f4103229d003702e
Yucao42 <cqcy3210@gmail.com>
2020-02-03 11:24:04 -0500

    fix some bug in grpc installation

--- grpc_install.sh
+++ grpc_install.sh
@@ -1,10 +1,12 @@
 
 set -ex
 
+num_threads=$(nproc)
+threads_used=$((num_threads/2))
+echo Using ${threads_used}
 git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
-git submodule update --update
 # git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
 # echo 'export LOCAL=${HOME}/packages' >> ${HOME}/.bashrc
 source ~/.bashrc
@@ -16,7 +18,7 @@ git checkout cares-1_15_0
 mkdir -p cmake/build
 cd cmake/build
 cmake -DCMAKE_BUILD_TYPE=Release ../..
-make -j24 DESTDIR=${LOCAL} install
+make -j${threads_used} DESTDIR=${LOCAL} install
 cd ../../../../..
 rm -rf third_party/cares/cares  # wipe out to prevent influencing the grpc build
 
@@ -25,7 +27,7 @@ cd third_party/zlib
 mkdir -p cmake/build
 cd cmake/build
 cmake -DCMAKE_BUILD_TYPE=Release ../..
-make -j24 DESTDIR=${LOCAL} install
+make -j${threads_used} DESTDIR=${LOCAL} install
 cd ../../../..
 rm -rf third_party/zlib  # wipe out to prevent influencing the grpc build
 
@@ -33,8 +35,8 @@ rm -rf third_party/zlib  # wipe out to prevent influencing the grpc build
 cd third_party/protobuf
 mkdir -p cmake/build
 cd cmake/build
-cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ..
-make -j24 DESTDIR=${LOCAL} install
+cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-fPIC" ..
+make -j${threads_used} DESTDIR=${LOCAL} install
 cd ../../../..
 rm -rf third_party/protobuf  # wipe out to prevent influencing the grpc build
 
@@ -42,7 +44,7 @@ rm -rf third_party/protobuf  # wipe out to prevent influencing the grpc build
 mkdir -p cmake/build
 cd cmake/build
 cmake -DgRPC_INSTALL=ON -DCMAKE_PREFIX_PATH=${LOCAL}/usr/local -DgRPC_BUILD_TESTS=OFF -DgRPC_PROTOBUF_PROVIDER=package -DgRPC_ZLIB_PROVIDER=package -DgRPC_CARES_PROVIDER=package -DgRPC_SSL_PROVIDER=package -DCMAKE_BUILD_TYPE=Release ../..
-make -j24 DESTDIR=${LOCAL} install
+make -j${threads_used} DESTDIR=${LOCAL} install
 cd ../..
 
 # Build helloworld example using cmake
